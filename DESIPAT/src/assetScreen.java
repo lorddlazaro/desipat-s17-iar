9c975bb509da8c7d87e266336892454c31e12007
lordd466b@gmail.com <lordd466b@gmail.com@00c36245-e8f2-f85e-e464-dc2a234d627f>
2013-07-01 10:05:11 +0000

    previous mistake of not joining values with lookup tables now fixed

--- DESIPAT/src/assetScreen.java
+++ DESIPAT/src/assetScreen.java
@@ -1,25 +1,14 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.Vector;
-
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.JLabel;
 import javax.swing.JTabbedPane;
-import java.awt.FlowLayout;
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JButton;
@@ -130,85 +119,85 @@ public class assetScreen extends JPanel {
 		JLabel label = new JLabel("Identifier:");
 		panel_4.add(label, "2, 2");
 		
-		lblIdentifier = new JLabel("Identifier:");
+		lblIdentifier = new JLabel("");
 		panel_4.add(lblIdentifier, "4, 2");
 		
 		JLabel label_7 = new JLabel("Maintenance Schedule:");
 		panel_4.add(label_7, "6, 2");
 		
-		lblMaintenance = new JLabel("Maintenance Schedule:");
+		lblMaintenance = new JLabel("");
 		panel_4.add(lblMaintenance, "8, 2");
 		
 		JLabel label_1 = new JLabel("Name:");
 		panel_4.add(label_1, "2, 4");
 		
-		lblName = new JLabel("Name:");
+		lblName = new JLabel("");
 		panel_4.add(lblName, "4, 4");
 		
 		JLabel label_8 = new JLabel("Financial Value:");
 		panel_4.add(label_8, "6, 4");
 		
-		lblFinancialValue = new JLabel("Financial Value:");
+		lblFinancialValue = new JLabel("");
 		panel_4.add(lblFinancialValue, "8, 4");
 		
 		JLabel label_2 = new JLabel("Owner:");
 		panel_4.add(label_2, "2, 6");
 		
-		lblOwner = new JLabel("Owner:");
+		lblOwner = new JLabel("");
 		panel_4.add(lblOwner, "4, 6");
 		
 		JLabel label_9 = new JLabel("Confidential Value:");
 		panel_4.add(label_9, "6, 6");
 		
-		lblConfidentialValue = new JLabel("Confidential Value:");
+		lblConfidentialValue = new JLabel("");
 		panel_4.add(lblConfidentialValue, "8, 6");
 		
 		JLabel label_3 = new JLabel("Custodian:");
 		panel_4.add(label_3, "2, 8");
 		
-		lblCustodian = new JLabel("Custodian:");
+		lblCustodian = new JLabel("");
 		panel_4.add(lblCustodian, "4, 8");
 		
 		JLabel label_10 = new JLabel("Integrity Value:");
 		panel_4.add(label_10, "6, 8");
 		
-		lblIntegrityValue = new JLabel("Integrity Value:");
+		lblIntegrityValue = new JLabel("");
 		panel_4.add(lblIntegrityValue, "8, 8");
 		
 		JLabel label_4 = new JLabel("Type:");
 		panel_4.add(label_4, "2, 10");
 		
-		lblType = new JLabel("Type:");
+		lblType = new JLabel("");
 		panel_4.add(lblType, "4, 10");
 		
 		JLabel label_11 = new JLabel("Availability Value:");
 		panel_4.add(label_11, "6, 10");
 		
-		lblAvailabilityValue = new JLabel("Availability Value:");
+		lblAvailabilityValue = new JLabel("");
 		panel_4.add(lblAvailabilityValue, "8, 10");
 		
 		JLabel label_5 = new JLabel("Date acquired:");
 		panel_4.add(label_5, "2, 12");
 		
-		lblDateAcquired = new JLabel("Date acquired:");
+		lblDateAcquired = new JLabel("");
 		panel_4.add(lblDateAcquired, "4, 12");
 		
 		JLabel label_13 = new JLabel("Storage Location:");
 		panel_4.add(label_13, "6, 12");
 		
-		lblClassification = new JLabel("Classification:");
+		lblClassification = new JLabel("");
 		panel_4.add(lblClassification, "8, 12");
 		
 		JLabel label_6 = new JLabel("Status:");
 		panel_4.add(label_6, "2, 14");
 		
-		lblStatus = new JLabel("Status:");
+		lblStatus = new JLabel("");
 		panel_4.add(lblStatus, "4, 14");
 		
 		JLabel label_12 = new JLabel("Classification:");
 		panel_4.add(label_12, "6, 14");
 		
-		lblStorageLocation = new JLabel("Storage Location:");
+		lblStorageLocation = new JLabel("");
 		panel_4.add(lblStorageLocation, "8, 14");
 		
 		db = new DBConnection();
@@ -275,7 +264,8 @@ public class assetScreen extends JPanel {
 		System.out.println("Selected asset's identifier: "+selectedIdentifier);
 		
 		db.open();
-		ResultSet rs = db.executeQuery(db.c,"select * from asset where identifier ="+selectedIdentifier+";");
+		ResultSet rs = db.executeQuery(db.c,"select identifier, name, Concat(P.firstName, ' ',P.middleInitial,'. ', P.lastName) as owner, Concat(C.firstName, ' ',C.middleInitial,'. ', C.lastName) as custodian, type,dateAcquired,status, M.maintSched,  financialValue, confidentialValue,integrityValue,availabilityValue, L.classification, S.storageLocation from asset A inner join person P on P.personID=A.ownerID inner join person C on C.personID=A.custodianID inner join typelookup T on T.typeID=A.typeID inner join maintenancelookup M on M.maintID=A.maintID inner join classificationlookup L on L.classID=A.classID inner join storage S on S.storageID=A.storageID where identifier="+selectedIdentifier);
+		
 		System.out.println("passed rs");
 		try {
 			while(rs.next()){
@@ -284,21 +274,20 @@ public class assetScreen extends JPanel {
 				//lblIdentifier.setText(rs.getString("identifier"));
 				lblIdentifier.setText(rs.getString("identifier"));
 				lblName.setText(rs.getString("name"));
+
+				lblOwner.setText(rs.getString("owner"));
 				
-				ResultSet owner = db.executeQuery(db.c, "select firstName + middleInitial + lastName as fullName from person where personID ="+rs.getInt("ownerID")+";");
-				lblOwner.setText(owner.getString("fullName"));
-				
-				lblCustodian.setText(rs.getString("custodianID"));
-				lblType.setText(rs.getString("typeID"));
+				lblCustodian.setText(rs.getString("custodian"));
+				lblType.setText(rs.getString("type"));
 				lblDateAcquired.setText(rs.getString("dateAcquired"));
 				lblStatus.setText(rs.getString("status"));
-				lblMaintenance.setText(rs.getString("maintID"));
+				lblMaintenance.setText(rs.getString("maintSched"));
 				lblFinancialValue.setText(rs.getString("financialValue"));
 				lblConfidentialValue.setText(rs.getString("confidentialValue"));
 				lblIntegrityValue.setText(rs.getString("integrityValue"));
 				lblAvailabilityValue.setText(rs.getString("availabilityValue"));
-				lblClassification.setText(rs.getString("classID"));
-				lblStorageLocation.setText(rs.getString("storageID"));
+				lblClassification.setText(rs.getString("classification"));
+				lblStorageLocation.setText(rs.getString("storageLocation"));
 				
 			}
 		} catch (SQLException e) {
