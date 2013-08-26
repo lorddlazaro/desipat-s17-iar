7f349961f1e8086552a5124774a0452c84d0c063
alron_lam@dlsu.ph <alron_lam@dlsu.ph@00c36245-e8f2-f85e-e464-dc2a234d627f>
2013-08-26 12:48:53 +0000

    Removed unnecessary classes (AddPerson Screens and Behavior)

--- DESIPAT/src/screenBehaviourStrategy/AddPersonScreenBehaviourStrategy.java
+++
@@ -1,9 +0,0 @@
-package screenBehaviourStrategy;
-
-import dataObjects.Person;
-
-
-public interface AddPersonScreenBehaviourStrategy {
-	public void cancel();
-	public void savePerson(Person p);
-}
